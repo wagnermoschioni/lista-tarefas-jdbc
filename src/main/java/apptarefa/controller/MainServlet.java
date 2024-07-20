714aa17dbb6c6e6bbc8497f441c51ad6afa4617a
wagnermoschioni <wagner.moschioni@gmail.com>
2024-07-20 20:34:26 -0300

    criacao MainServlet e insercao de imagens

--- src/main/java/apptarefa/controller/MainServlet.java
+++ src/main/java/apptarefa/controller/MainServlet.java
@@ -1,6 +1,7 @@
 package apptarefa.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet
-public class Controller extends HttpServlet{
+@WebServlet("/main")
+public class MainServlet extends HttpServlet{
 
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><body>Teste Servlet Main</body></html>");
+		
 	}
 }
